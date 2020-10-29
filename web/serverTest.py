06ec6cd77b792440f032aa09f298996657a3c778
Александр Валерьевич Медведев <amedvedev@stc-spb.ru>
2020-10-29 20:16:48 +0500

    -dev

--- web/serverTest.py
+++ web/serverTest.py
@@ -1,5 +1,40 @@
 from flask import Flask
+import flask_login
+
 app = Flask(__name__)
+app.secret_key = 'super secret string'
+
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
 
 @app.route('/')
 def hello_world():
