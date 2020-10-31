db8ac5c55c9f3e23f72a265bf7cf62af6a9890fa
Tyill <al_medved@mail.ru>
2020-10-31 23:43:08 +0500

    -dev

--- web/backend/__init__.py
+++ web/backend/__init__.py
@@ -25,15 +25,12 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-   
-    from . import db
-    db.init_app(app) 
-
+       
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+    # from . import blog
+    # app.register_blueprint(blog.bp)
+    # app.add_url_rule('/', endpoint='index')
 
     return app
