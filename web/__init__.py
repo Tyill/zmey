68a4185f6e12634dc7bec911f4ebc5633cadccf2
Александр Валерьевич Медведев <amedvedev@stc-spb.ru>
2020-11-01 13:30:51 +0500

    -dev

---
+++ web/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import os
+
+def create_app():
+  app = Flask(__name__, instance_relative_config=True, 
+                        static_url_path='', 
+                        static_folder='',
+                        template_folder='templates') 
+  app.config.from_mapping(
+      SECRET_KEY='dev',
+  )
+  app.config.from_pyfile('config.cfg', silent=True) 
+
+  try:
+      print(app.config) 
+      os.makedirs(app.instance_path)
+  except OSError:
+      pass
+      
+  from . import auth
+  app.register_blueprint(auth.bp)
+
+  # from . import blog
+  # app.register_blueprint(blog.bp)
+  # app.add_url_rule('/', endpoint='index')
+
+  return app
